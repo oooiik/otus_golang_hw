b1c8528974a595486cb23bc5cfc0606e3af32972
oooiik <toshevobidjon@gmail.com>
2023-02-03 21:48:26 +0500

    HW02 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,42 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	st := ""
+	if 0 >= len(s) {
+		return st, nil
+	}
+	var lastV int32
+	for _, valS := range s {
+		if unicode.IsNumber(valS) {
+
+			num, err := strconv.Atoi(string(valS))
+			if err != nil {
+				return "", ErrInvalidString
+			}
+
+			if lastV == 0 {
+				return "", ErrInvalidString
+			}
+
+			if num == 0 {
+				st = st[:len(st)-1]
+			} else {
+				st = st + strings.Repeat(string(lastV), num-1)
+			}
+			lastV = 0
+		} else {
+			lastV = valS
+			st += string(lastV)
+		}
+	}
+
+	return st, nil
 }
