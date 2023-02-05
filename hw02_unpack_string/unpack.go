1975ab565f0fedbbf39c28cff64e3216ebc8fb08
oooiik <toshevobidjon@gmail.com>
2023-02-05 18:53:19 +0500

    HW02 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -8,27 +8,53 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(s string) (string, error) {
-	var res = strings.Builder{}
+type char struct {
+	IsPunct bool
+	Value   rune
+}
 
-	for key, char := range s {
+func Unpack(s string) (string, error) {
+	// generate map
+	var mapChar []char
+
+	for key := 0; key < len(s); key++ {
+		curr := rune(s[key])
+
 		var next rune
 		if len(s) > key+1 {
 			next = rune(s[key+1])
 		}
+		if unicode.IsPunct(curr) {
+			mapChar = append(mapChar, char{true, next})
+			key++
+			continue
+		}
+		mapChar = append(mapChar, char{false, curr})
+	}
 
-		if unicode.IsLetter(char) {
-			if unicode.IsNumber(next) {
-				res.WriteString(strings.Repeat(string(char), int(next-'0')))
-			} else {
-				res.WriteRune(char)
-			}
-		} else if unicode.IsNumber(char) {
-			if res.Len() == 0 || unicode.IsNumber(next) {
-				return "", ErrInvalidString
-			}
+	// generate string
+	var res = strings.Builder{}
+	for keyChar := 0; keyChar < len(mapChar); keyChar++ {
+		currChar := mapChar[keyChar]
+
+		var nextChar char
+		if len(mapChar) > keyChar+1 {
+			nextChar = mapChar[keyChar+1]
+		} else {
+			res.WriteRune(currChar.Value)
+			break
 		}
 
+		if unicode.IsNumber(currChar.Value) && !currChar.IsPunct {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsNumber(nextChar.Value) && !nextChar.IsPunct {
+			res.WriteString(strings.Repeat(string(currChar.Value), int(nextChar.Value-'0')))
+			keyChar++
+		} else {
+			res.WriteRune(currChar.Value)
+		}
 	}
 
 	return res.String(), nil
