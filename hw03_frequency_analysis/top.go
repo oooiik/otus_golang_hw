c118a514beb47fe9d2879889579c06d59e92075b
oooiik <toshevobidjon@gmail.com>
2023-02-14 19:01:58 +0500

    HW03 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,46 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(s string) []string {
+	if len(s) == 0 {
+		return nil
+	}
+
+	s = strings.ReplaceAll(s, "\n", " ")
+	s = strings.ReplaceAll(s, "\t", "")
+
+	otchot := make(map[string]int)
+	for _, slova := range strings.Fields(s) {
+		if len(slova) == 0 {
+			continue
+		}
+		_, has := otchot[slova]
+
+		if has {
+			otchot[slova]++
+		} else {
+			otchot[slova] = 1
+		}
+	}
+
+	keys := make([]string, 0)
+
+	for key := range otchot {
+		keys = append(keys, key)
+	}
+
+	sort.Strings(keys)
+	sort.SliceStable(keys, func(i, j int) bool {
+		return otchot[keys[i]] > otchot[keys[j]]
+	})
+
+	if len(keys) <= 10 {
+		return keys
+	}
+
+	return keys[:10]
 }
