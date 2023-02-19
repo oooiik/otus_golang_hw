b01b2a871a44e5967a4893d52bdcba7699d4b97e
oooiik <toshevobidjon@gmail.com>
2023-02-19 12:55:10 +0500

    HW04 list

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,117 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	ele *ListItem
+}
+
+func (l *list) Len() (i int) {
+	i = 0
+	if l.ele == nil {
+		return
+	}
+	l.ele = l.Front()
+	for {
+		i++
+		if l.ele.Next == nil {
+			return
+		}
+		l.ele = l.ele.Next
+	}
+}
+
+func (l *list) Front() *ListItem {
+	if l.ele == nil {
+		return nil
+	}
+	for {
+		if l.ele.Prev == nil {
+			return l.ele
+		}
+		l.ele = l.ele.Prev
+	}
+}
+
+func (l *list) Back() (ele *ListItem) {
+	if l.ele == nil {
+		return nil
+	}
+	for {
+		if l.ele.Next == nil {
+			return l.ele
+		}
+		l.ele = l.ele.Next
+	}
+}
+
+func (l *list) PushFront(v interface{}) (li *ListItem) {
+	li = &ListItem{
+		Value: v,
+		Next:  nil,
+		Prev:  nil,
+	}
+	if l.Len() == 0 {
+		l.ele = li
+		return
+	}
+	li.Next = l.Front()
+	l.Front().Prev = li
+	return
+}
+
+func (l *list) PushBack(v interface{}) (li *ListItem) {
+	li = &ListItem{
+		Value: v,
+		Next:  nil,
+		Prev:  nil,
+	}
+	if l.Len() == 0 {
+		l.ele = li
+		return
+	}
+	li.Prev = l.Back()
+	l.Back().Next = li
+	return
+}
+
+func (l *list) Remove(i *ListItem) {
+	defer func() {
+		i.Next, i.Prev = nil, nil
+	}()
+
+	if i.Next == nil && i.Prev == nil {
+		l.ele = nil
+		return
+	}
+
+	if i.Next == nil {
+		i.Prev.Next = nil
+		l.ele = i.Prev
+		return
+	}
+
+	if i.Prev == nil {
+		i.Next.Prev = nil
+		l.ele = i.Next
+		return
+	}
+
+	i.Next.Prev = i.Prev
+	i.Prev.Next = i.Next
+	l.ele = i.Prev
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	l.Remove(i)
+	i.Next = nil
+	i.Prev = nil
+
+	if l.Len() == 0 {
+		l.ele = i
+		return
+	}
+
+	i.Next = l.Front()
+	l.Front().Prev = i
 }
 
 func NewList() List {
