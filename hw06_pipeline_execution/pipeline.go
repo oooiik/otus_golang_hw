78b1631d785a6f545d2cc12c9b6f51bcbc9a54df
oooiik <toshevobidjon@gmail.com>
2023-04-12 15:24:27 +0500

    HW06 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,35 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+
+	sChan := make([]chan interface{}, len(stages))
+
+	runner := func(stage Stage, inner <-chan interface{}, outer chan<- interface{}) {
+		defer close(outer)
+		inValues := stage(inner)
+		for {
+			select {
+			case <-done:
+				return
+			case inValue, open := <-inValues:
+				if !open {
+					return
+				}
+				outer <- inValue
+			}
+		}
+	}
+
+	for k, stage := range stages {
+		sChan[k] = make(chan interface{})
+		go func(k int, stage Stage) {
+			if k == 0 {
+				runner(stage, in, sChan[k])
+			} else {
+				runner(stage, sChan[k-1], sChan[k])
+			}
+		}(k, stage)
+	}
+
+	return sChan[len(stages)-1]
 }
